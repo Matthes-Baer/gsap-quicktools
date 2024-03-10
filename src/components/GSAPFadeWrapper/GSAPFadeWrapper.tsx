6c9f5e081a567f6256c7954d5587685e1157adca
Matthes Bär <Matthes.baer@yahoo.de>
2024-03-10 15:35:55 +0100

    init

---
+++ src/components/GSAPFadeWrapper/GSAPFadeWrapper.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactNode } from "react";
+import gsap from "gsap";
+import useGSAP from "@gsap/react";
+
+export default function GSAPFadeWrapper() {
+  return <div>This is some testing</div>;
+}
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as GSAPFadeWrapper } from "./GSAPFadeWrapper";
